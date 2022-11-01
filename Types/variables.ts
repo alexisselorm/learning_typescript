eb978ce0070d4841ef601140ea12c51f6ab93abb
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-01 00:29:21 +0000

    Types

--- Types/variables.ts
+++ Types/variables.ts
@@ -10,3 +10,30 @@ catLives+=1;
 let gameOver: boolean = false
 gameOver=true
 // gameOver="false"
+
+
+// Type inference
+let tvShow = "Oliver Twist"
+tvShow = "Bleach";
+tvShow=false;
+
+let isFunny = false
+isFunny = true
+isFunny = "ASD"
+
+// Any type
+let anyVar: any = true
+anyVar = false
+anyVar = 1
+anyVar = "1"
+anyVar = [1,2,3]
+anyVar = [1,2,3]
+anyVar = [1,2,3]
+
+const movies = ["Arrival", "The mask","Amadeus","Odyssey"]
+let foundMovie: string;
+
+for(let movie of movies){
+    if(movie ==="Amadeus")
+    foundMovie = movie;
+}
